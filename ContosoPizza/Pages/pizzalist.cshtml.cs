febabc53f71d6ffd17a8776aedca231c9ce5accc
KEVIN DOS SANTOS SILVA <kevin.ssilva8@senacsp.edu.br>
2023-07-27 17:20:32 -0300

    atualização 27/07/23

--- ContosoPizza/Pages/pizzalist.cshtml.cs
+++ ContosoPizza/Pages/pizzalist.cshtml.cs
@@ -8,6 +8,8 @@ namespace ContosoPizza.Pages
 {
     public class pizzalistmodel : PageModel
     {
+        [BindProperty]
+        public Pizza NewPizza { get; set; } = default!;
         private readonly PizzaService _Service;
         public IList<Pizza> pizzalist { get; set; } = default!;
 
@@ -19,5 +21,23 @@ namespace ContosoPizza.Pages
         {
             pizzalist = _Service.GetPizzas();
         }
+
+        public IActionResult Onpost()
+        {
+            if(!ModelState.IsValid ||  NewPizza == null)
+            {
+                return Page();
+            }
+            _Service.AddPizza(NewPizza);
+            return RedirectToAction("Get");
+        }
+        public IActionResult OnPostDelete(int id)
+        {
+            _Service.DeletePizza(id);
+
+            return RedirectToAction("get");
+
+        }
+
     }
 }
